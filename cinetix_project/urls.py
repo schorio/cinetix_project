44faca40f12ac99d83557caca539a8c7739cdac9
schorio <schorioignace@gmail.com>
2024-03-09 05:25:34 +0100

    mise en place d'url pour login

--- cinetix_project/urls.py
+++ cinetix_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import login_page
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login_page, name='login'),
 ]
