348533ac1e74c73e1545f6dad3c3d499dec3335e
schorio <schorioignace@gmail.com>
2024-03-09 04:22:31 +0100

    creation du model de film

--- cinetix/models.py
+++ cinetix/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Film(models.Model):
+    titre = models.CharField(max_length=255, unique=True)
+    categorie = models.CharField(max_length=255, unique=True)
+    synopsis = models.CharField(max_length=255, unique=True)
+    acteur = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.titre
+    
+    def get_absolute_url(self):
+        return reverse('film-list')
