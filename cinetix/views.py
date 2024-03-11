e49f985402e72320a109b1e2939de20c143b47b7
schorio <schorioignace@gmail.com>
2024-03-11 07:59:39 +0100

    creation du views pour film

--- cinetix/views.py
+++ cinetix/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+)
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import (
+    Film
+)
+
+class FilmListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
+    model = Film
+    template_name = 'cinetix/film/film_list.html'
+    context_object_name = 'films'
