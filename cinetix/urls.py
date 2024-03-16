b9062036a3adf42ae7ecd93d31d672db6ce12efc
schorio <schorioignace@gmail.com>
2024-03-16 10:56:55 +0100

    ajout d'url pour creer un nouveau film

--- cinetix/urls.py
+++ cinetix/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     FilmListView,
+    FilmCreateView
 )
 
 app_name = "cinetix"
 
 urlpatterns = [
-    path('film-list/', FilmListView.as_view(), name='film-list')
+    path('film-list/', FilmListView.as_view(), name='film-list'),
+    path('film-list/nouveau/', FilmCreateView.as_view(), name='film-create')
 ]
