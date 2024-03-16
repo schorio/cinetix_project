55233b2a09669f9caa3003fe67af334b62458d0c
schorio <schorioignace@gmail.com>
2024-03-16 11:14:44 +0100

    ajout d'url pour creer un nouveau film

--- cinetix/urls.py
+++ cinetix/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import (
     FilmListView,
     FilmCreateView,
-    FilmUpdateView
+    FilmUpdateView,
+    FilmDeleteView
 )
 
 app_name = "cinetix"
@@ -11,5 +12,6 @@ app_name = "cinetix"
 urlpatterns = [
     path('film-list/', FilmListView.as_view(), name='film-list'),
     path('film-list/nouveau/', FilmCreateView.as_view(), name='film-create'),
-    path('film-list/<int:pk>/modification/', FilmUpdateView.as_view(), name='film-update')
+    path('film-list/<int:pk>/modification/', FilmUpdateView.as_view(), name='film-update'),
+    path('film-list/<int:pk>/suppresion/', FilmDeleteView.as_view(), name='film-delete'),
 ]
