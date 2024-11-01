28ec11f68322f17674e9127aedfd99483fe81298
schorio <schorioignace@gmail.com>
2024-11-01 07:29:25 +0300

    Setting : modification du db en postgresql

--- cinetix_project/settings.py
+++ cinetix_project/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'cinetix_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinetix_db',
+        'USER': 'postgres',
+        'PASSWORD': 'schorio',
+        'HOST': 'localhost'
     }
 }
 
