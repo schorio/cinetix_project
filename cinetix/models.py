780ab440e74c58d73909be8b5a839f299e8531db
schorio <schorioignace@gmail.com>
2024-11-01 10:20:44 +0300

    model : creation du model pour les projections

--- cinetix/models.py
+++ cinetix/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.postgres.fields import ArrayField
 
 class Film(models.Model):
     id_film = models.AutoField(primary_key=True)
@@ -17,3 +18,22 @@ class Film(models.Model):
     
     def get_absolute_url(self):
         return reverse('film-list')
+    
+    
+    
+class Projection(models.Model):
+    id_proj = models.AutoField(primary_key=True)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name="projections")
+    type_proj = models.CharField(max_length=255)
+    timings = models.CharField(max_length=255)
+    place_proj = ArrayField(
+        models.CharField(max_length=5),
+        size=92,  # Taille maximale du tableau
+        blank=True,
+        default=list,  # Valeur par défaut pour le champ
+    )
+    paf = models.FloatField()
+    description = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.film
