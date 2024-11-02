2ae135f969964c525eb7587892a3068e31ccf4a9
schorio <schorioignace@gmail.com>
2024-11-02 08:25:45 +0300

    forms : mise en forme

--- cinetix/forms.py
+++ cinetix/forms.py
@@ -1,18 +1,8 @@
 from django import forms
-
 from .models import Film
 
-
-class film_form(forms.ModelForm):
-
+class FilmForm(forms.ModelForm):
 
     class Meta:
         model = Film
-        fields = [
-            'titre', 'genres', 'acteur', 'synopsis',
-            'evaluation', 'trailer', 'posteur', 'commentaires'      
-        ]
-        # widgets = {
-        #     'start_date': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
-        #     'end_date': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
-        # }
+        fields = ['titre', 'evaluation', 'genres', 'acteur', 'synopsis', 'trailer', 'posteur', 'commentaires']
