26e52904def65cfd07b09a969f7a009e974e74de
schorio <schorioignace@gmail.com>
2024-11-10 04:26:04 +0300

    Views : ajout des views pour les afficher et creer des projections

--- cinetix/views.py
+++ cinetix/views.py
@@ -9,10 +9,8 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from .models import (
-    Film
-)
+from .models import *
+from .forms import *
 
 class FilmListView(LoginRequiredMixin, ListView):
     login_url = 'login'
@@ -41,3 +39,23 @@ class FilmDeleteView(LoginRequiredMixin, DeleteView):
     model =  Film
     pk_url_kwarg = 'id_film'
     success_url = reverse_lazy('cinetix:film-list')
+    
+
+class ProjListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
+    model = Projection
+    template_name = 'cinetix/projection/fc_list.html'
+    context_object_name = 'projection'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProjectionForm()  # Ajouter le formulaire de création
+        return context
+    
+
+class ProjCreateView(LoginRequiredMixin, CreateView):
+    login_url = 'login'
+    model = Projection
+    form_class = ProjectionForm
+    template_name = 'cinetix/projection/proj_create.html'  # Votre template HTML pour ce formulaire
+    success_url = reverse_lazy('cinetix:projection-list')  # Redirige vers la liste des projections après la soumission
